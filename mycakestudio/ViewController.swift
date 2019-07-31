3383aeea0194d66a09d237476dd77a3ca53a45a6
cdanqian <c.dan.qian@gmail.com>
2019-07-31 12:43:48 +0800

    Initial Commit

---
+++ mycakestudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mycakestudio
+//
+//  Created by 千丹 on 2019/7/31.
+//  Copyright © 2019 dyq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mycakestudioTests/mycakestudioTests.swift
@@ -0,0 +1,34 @@
+//
+//  mycakestudioTests.swift
+//  mycakestudioTests
+//
+//  Created by 千丹 on 2019/7/31.
+//  Copyright © 2019 dyq. All rights reserved.
+//
+
+import XCTest
+@testable import mycakestudio
+
+class mycakestudioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
